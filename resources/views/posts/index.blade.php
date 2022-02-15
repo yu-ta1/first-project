bc492f37bbfa8f67a650e34bea66d0fc302878d6
yu-ta1 <youtaijintian10@gmail.com>
2022-02-15 09:21:35 +0900

    記事詳細画面

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -11,10 +11,17 @@
         <div class='posts'>
             @foreach ($posts as $post)
                 <div class='post'>
-                    <h2 class='title'>{{ $post->title }}</h2>
-                    <p class='body'>{{ $post->body }}</p>
+                    <h2 class='title'>
+                         <a href="/posts/{{ $post->id }}">{{ $post->title }}</a>
+                    </h2>
+                    <p class='body'>
+                        {{ $post->body }}
+                    </p>
                 </div>
             @endforeach
+        </div>
+        <div class='paginate'>
+            {{ $posts->links() }}
         </div>
     </body>
 </html>
