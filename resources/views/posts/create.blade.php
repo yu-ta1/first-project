7b2aa074006b0252f5f5ecdd76471ca277927107
yu-ta1 <youtaijintian10@gmail.com>
2022-03-09 10:05:15 +0900

    カリキュラム09-3完了

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,3 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -23,3 +26,4 @@
         <div class="back">[<a href="/">back</a>]</div>
     </body>
 </html>
+@endsection
--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -1,3 +1,6 @@
+@extends('layouts.app')
+
+@section('content')
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
     <head>
@@ -23,3 +26,4 @@
         </div>
     </body>
 </html>
+@endsection
